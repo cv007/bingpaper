3f01d0614bb38dd2cc487362b090fac3f35029b7
cv007 <32439630+cv007@users.noreply.github.com>
2019-11-25 22:50:44 -0600

    Update bingpaper.sh

--- bingpaper.sh
+++ bingpaper.sh
@@ -12,7 +12,7 @@ cd $(dirname $0)
 #if any commandline option passed, then just show a random picture
 [ -n "$1" ] && img=$(ls *.jpg|sort -R|tail -n 1) ||
     #otherwise get wallpaper url
-    img=$(wget -t2 -T10 -qO - $url|sed 's:["\x27&\\]:\n:g'|grep -m1 "^/.*\.jpg")
+    img=$(wget -t2 -T10 -qO - $url|sed 's:["\x27&\\]:\n:g'|grep -m1 "^/th?id=.*1080\.jpg")
     #/th?id=OHR.PeelCastle_EN-US6180948507_1920x1080.jpg
 
 #if img and no commandline option, download wallpaper
